a6838d4f3fb8aa35b0f2f439a7f7fa95bdc615b0
vytautasD <vytautas.demenius@gmail.com>
2017-10-20 18:31:26 +0300

    Ikonos

--- resources/views/startwindow.blade.php
+++ resources/views/startwindow.blade.php
@@ -7,32 +7,32 @@
 @section('content')
     <div class="container">
         <a href="{{ route('sarasas') }}" class="menu-btn a">
-            <span class="icon icon-gear"></span>
+            <i class="fa fa-list" aria-hidden="true"></i>&nbsp;
             <span class="right"></span>Sąrašas
         </a>
 
         <a href="{{ route('gydymai') }}" class="menu-btn b">
-            <span class="icon icon-gear"></span>
+            <i class="fa fa-medkit" aria-hidden="true"></i>&nbsp;
             <span class="right"></span>Gydymas
         </a>
 
         <a href="{{ route('ruja') }}" class="menu-btn c">
-            <span class="icon icon-gear"></span>
+            <i class="fa fa-heart" aria-hidden="true"></i>&nbsp;
             <span class="right"></span>Ruja
         </a>
 
         <a href="{{ route('medikamentai', ["category" => 1]) }}" class="menu-btn d">
-            <span class="icon icon-gear"></span>
+            <i class="fa fa-user-md" aria-hidden="true"></i>&nbsp;
             <span class="right"></span>V. Rasimo medikamentai
         </a>
 
         <a href="{{ route('medikamentai', ["category" => 2]) }}" class="menu-btn d">
-            <span class="icon icon-gear"></span>
+            <i class="fa fa-user-md" aria-hidden="true"></i>&nbsp;
             <span class="right"></span>R. Knašio medikamentai
         </a>
 
         <a href="{{ route('heat.calving.index') }}" class="menu-btn e">
-            <span class="icon icon-gear"></span>
+            <i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;
             <span class="right"></span>Veršiavimaisi ({{ GeneralHelper::getCalvingsThisMonthCount() }})
         </a>
     </div>
